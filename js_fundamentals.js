036e88b1c68caa4e8b09cb5001f77d3596530c2a
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-07 10:21:29 +0000

    some code added

--- js_fundamentals.js
+++ js_fundamentals.js
@@ -1,1 +1,25 @@
-console.log("Kidda Dunya?");
+/*
+
+var - globally scoped
+let - locally scoped, mutable
+const - locally scoped, immutable
+
+*/
+
+var name = "Kidda";
+console.log('name:', name)
+
+// Dynamically typed - reassign the same variable to a different type.
+name = 1;
+console.log('name is now:', name)
+
+// null is an INTENTIONAL nothing
+
+var mango = null;
+console.log('Mango:', mango)
+
+
+// undefined is an UNINTENTIONAL nothing, e.g., you defined a variable but not assigned a value to it
+
+var apple;
+console.log('Apple:', apple)
