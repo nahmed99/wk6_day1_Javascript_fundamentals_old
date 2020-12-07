7dbd3898424aca1dced2c72dfa14d8121b6f8bff
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-07 11:24:29 +0000

    Array code

--- obj_array_loops/object_arrays_loops.js
+++ obj_array_loops/object_arrays_loops.js
@@ -4,3 +4,22 @@ var sports = ["football", "tennis", "rugby"];
 var numberOfElements = sports.length; //NOTE: length is a property, NOT a method
 
 console.log("Number of elements: ", numberOfElements)
+
+
+// Add elements to an array
+sports.push("kabadi");
+sports.push("gooli-danda");
+sports.push("ludo");
+
+console.log('Sport:', sports);
+
+
+// Remove stuff from the array:
+
+//remove last element, and return it to use if you need to...
+console.log("Just removed", sports.pop()); 
+
+//remove element from the beginning of the array:
+console.log("Just removed", sports.shift()); 
+
+console.log('Sport:', sports);
