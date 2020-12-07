4cb6ba1cbb898090233a9800864cd10b5c55cda4
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-07 12:09:35 +0000

    object created and values accessed

---
+++ obj_array_loops/objects.js
@@ -0,0 +1,27 @@
+/*
+
+Objects in JS are key-value pairs, used to store data.
+
+Simple way to create an object:
+
+    {}
+
+
+For example:
+
+var movie = {};     //That's us created an object!
+console.log("movie:", movie);
+
+*/
+
+
+var movie = {
+    title: "The Big Game",
+    year: 2020,
+    language: 'Swahili'
+};
+
+// console.log("movie:", movie);
+
+console.log("Movie title:", movie.title);
+console.log("Movie year:", movie.year);
