9c5952e17089fbd3a9fa188dd5fd36b922cc37fc
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-07 11:14:13 +0000

    object, arrays and loops folder created

---
+++ obj_array_loops/object_arrays_loops.js
@@ -0,0 +1,6 @@
+// Arrays
+var sports = ["football", "tennis", "rugby"];
+
+var numberOfElements = sports.length; //NOTE: length is a property, NOT a method
+
+console.log("Number of elements: ", numberOfElements)
