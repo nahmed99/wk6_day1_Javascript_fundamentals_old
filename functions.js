723f3f5f9fda6cdfd3f6d45f61b34bce14dfb3fa
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-07 14:12:48 +0000

    Coded named and anonymous functions

--- functions.js
+++ functions.js
@@ -1,5 +1,21 @@
-function sayHello() {
-    return "Kidda?";
+//Hoisting; you can call your function before the place where it is defined.
+
+
+// (1) Named Functions
+function sayHello(greeting, name="Dunya") {
+    return `Kidda, ${greeting} ${name}`;
 }
 
 console.log("sayHello message:", sayHello());
+console.log("sayHello message:", sayHello('happy holiday'));
+console.log("sayHello message:", sayHello('happy holiday', 'yaar'));
+
+
+// (2) Anonymous Function Expressions - these functions can have a semi-colon after the last curly-brace. Normal functions don't need that.
+// These functions are not  hoisted.
+
+var add = function(firstNum, secondNum) {
+    return firstNum + secondNum;
+};
+
+console.log("1 + 3 is", add(1, 3));
